c8508317b7c926a02a015c6987bb296ddd70b0eb
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-12-26 02:41:02 +0100

    Fixed some timeUtil

--- src/ElPricesModular.cpp
+++ src/ElPricesModular.cpp
@@ -4,6 +4,10 @@
 
 #include "ElPricesModular.h"
 
+#include <iostream>
+
 ElPricesModular::ElPricesModular() : elPricesCollector_(std::make_shared<ElPricesCollector>())
 {
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+    std::cout << elPricesCollector_->getCurrentPrice()->getPriceWithoutFees() << std::endl;
 }
