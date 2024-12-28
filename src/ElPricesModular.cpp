92d9a2412540f1e2ccee92afc78414222f0dc1ba
EAL <elias@takefive.dk>
2024-12-28 01:09:14 +0100

    Limited to 2 decimalPoints

--- src/ElPricesModular.cpp
+++ src/ElPricesModular.cpp
@@ -17,18 +17,15 @@ void ElPricesModular::launch()
 {
     InitWindow(1280, 720, "ElPricesModular");
     SetTargetFPS(10);
-    std::string currentUsageAsPrice;
     while (not WindowShouldClose())
     {
         const double currentPrice = elPricesCollector_->getCurrentPrice()->getPriceWithoutFees();
         const double pulses = elPriceUsageController_->getPulses();
         const double kwhUsed = pulses / 1000;
         const double currentIntervalPrice = currentPrice * kwhUsed;
-
-        currentUsageAsPrice = std::to_string(currentIntervalPrice);
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText(currentUsageAsPrice.c_str(), 560, 310, 50, BLUE);
+        DrawText(TextFormat("%.2f",currentIntervalPrice), 560, 310, 50, BLUE);
         EndDrawing();
     }
 }
