eefd09befe5735e7a58e17c33afb4fe478054ac9
EAL <elias@takefive.dk>
2024-12-31 13:45:50 +0100

    main now loads config

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include "ElPricesModular.h"
+#include "Utility/ConfigController.h"
 
 int main()
 {
+    ConfigController::loadConfig("../../Resources/config.json");
     auto elPricesModular = std::make_unique<ElPricesModular>();
     elPricesModular->launch();
     return 0;
