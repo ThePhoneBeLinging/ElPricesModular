cbe80b7bb59e2aad438029b79181347184a8942c
EAL <elias@takefive.dk>
2025-01-28 15:00:50 +0100

    Build and run now makes sure its up to date before running

--- scripts/buildAndRun.py
+++ scripts/buildAndRun.py
@@ -3,6 +3,8 @@ import subprocess
 
 if not os.path.exists("build"):
         os.makedirs("build")
+subprocess.run(["git","pull"])
+subprocess.run(['python3',"scripts/loadExternalsSSH.py"], check=True)
 subprocess.run(['cmake',".."], cwd = "build", check=True)
 subprocess.run(['make'], cwd = "build", check=True)
 subprocess.run(['./ElPricesModular'], cwd = "build/src", check=True)
