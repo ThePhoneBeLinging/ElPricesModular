279c8fb6f013951a2c7271fe59e0053db4423381
EAL <elias@takefive.dk>
2025-01-28 15:08:59 +0100

    Added export call

--- scripts/buildAndRun.py
+++ scripts/buildAndRun.py
@@ -4,6 +4,7 @@ import subprocess
 if not os.path.exists("build"):
         os.makedirs("build")
 subprocess.run(["git","pull"])
+subprocess.run(["export MESA_GL_VERSION_OVERRIDE=4.5"], check = True)
 subprocess.run(['python3',"scripts/loadExternalsSSH.py"], check=True)
 subprocess.run(['cmake',".."], cwd = "build", check=True)
 subprocess.run(['make'], cwd = "build", check=True)
