09feb80d83679e891f43466083f0c23b83f05648
EAL <elias@takefive.dk>
2025-02-04 19:15:00 +0100

    added shell = true

--- scripts/buildAndRun.py
+++ scripts/buildAndRun.py
@@ -4,7 +4,7 @@ import subprocess
 if not os.path.exists("build"):
         os.makedirs("build")
 subprocess.run(["git","pull"], check = True)
-subprocess.run(["export MESA_GL_VERSION_OVERRIDE=4.5"], check = True)
+subprocess.run(["export MESA_GL_VERSION_OVERRIDE=4.5"], shell = True, check = True)
 subprocess.run(['python3',"scripts/loadExternalsSSH.py"], check=True)
 subprocess.run(['cmake',".."], cwd = "build", check=True)
 subprocess.run(['make'], cwd = "build", check=True)
