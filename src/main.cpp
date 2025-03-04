4054ee2d7e8fc81cbc3d0986482c934592d40faf
EAL <elias@takefive.dk>
2025-03-04 13:40:41 +0100

    encased program in try catch

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
+
 #include "ElPricesModular.h"
 #include "Utility/ConfigController.h"
+#include "Utility/DebugController.h"
 
 int main()
 {
-    ConfigController::loadConfig("../../Resources/config.json");
-    auto elPricesModular = std::make_unique<ElPricesModular>();
-    elPricesModular->launch();
+    try
+    {
+        ConfigController::loadConfig("../../Resources/config.json");
+        auto elPricesModular = std::make_unique<ElPricesModular>();
+        elPricesModular->launch();
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    DebugController::dumpDebug("../../debug.txt");
     return 0;
 }
