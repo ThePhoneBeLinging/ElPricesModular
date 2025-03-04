e39d31cdad1ad91c2a1f80ca16f2cc244f64d596
EAL <elias@takefive.dk>
2025-03-04 22:17:57 +0100

    made attempt at update

--- scripts/buildAndRun.py
+++ scripts/buildAndRun.py
@@ -4,6 +4,7 @@ import subprocess
 if not os.path.exists("build"):
         os.makedirs("build")
 subprocess.run(["git","pull"], check = True)
+subprocess.run(["export DISPLAY=:0"], shell = True, check = True)
 subprocess.run(["export MESA_GL_VERSION_OVERRIDE=4.5"], shell = True, check = True)
 subprocess.run(['python3',"scripts/loadExternals.py"], check=True)
 subprocess.run(['python3',"scripts/updateExternals.py"], check=True)
