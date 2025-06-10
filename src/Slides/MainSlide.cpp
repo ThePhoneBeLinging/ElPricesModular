6afbbcc5f2b505684ae00d33762d64f6d3ffc9e3
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-06-10 15:35:30 +0200

    fixed cause of undefined error

--- src/Slides/MainSlide.cpp
+++ src/Slides/MainSlide.cpp
@@ -135,6 +135,10 @@ MainSlide::MainSlide(const std::shared_ptr<ElPricesCollector>& collectorControll
             json["Box2"] = largePriceGroupColumns_[1]->getTexts();
             json["Box3"] = largePriceGroupColumns_[2]->getTexts();
             json["Box4"] = largePriceGroupColumns_[3]->getTexts();
+
+            std::string string = fmt::format("{:.2f} Kr",price / 10000);
+            json["CurrentPrice"] = string;
+
             DataController::setPriceJSONObject(json);
             int delay = TimeUtil::secondsToNextHour();
             condVar_.wait_for(lock,std::chrono::seconds(delay));
@@ -193,9 +197,6 @@ MainSlide::MainSlide(const std::shared_ptr<ElPricesCollector>& collectorControll
             double price = collectorController->getCurrentPrice()->getTotalPrice();
             std::string string = fmt::format("{:.2f} Kr",price / 10000);
             text->setText(string);
-            nlohmann::json json = DataController::getPriceJSONObject();
-            json["CurrentPrice"] = string;
-            DataController::setPriceJSONObject(json);
             int secondsToWait = TimeUtil::secondsToNextHour();
             condVar_.wait_for(lock,std::chrono::seconds(secondsToWait));
         }
